921d30c61144e4d80b051a052b3c55fc25d5f1c2
Robert Koor <robertkoor0@gmail.com>
2026-03-12 23:31:57 +0200

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: 'https://github.com/Rbr4t/Statistikaamet-proovit-',
-  plugins: [react()],
-})
